5bd11a8c1c8f360de615834a31a0a39d2f5b0733
Sayan Shankhari <sayan0shankhari@gmail.com>
2026-05-24 15:59:27 +0530

    chore(misc): architecture change
    Rename and moved some folders.

---
+++ 01-MyYoutube/backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use (cors());
+app.use (express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen (port, () => {
+	console.log (`Server is running on port ${port}`);
+});
+
+app.get ("/", (_request, response) => {
+	response.send ("Hello, World!");
+});
